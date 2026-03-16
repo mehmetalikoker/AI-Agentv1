5b4e316b6e196978e36c8373c5b4de834f88d036
Mehmet <mehmetalikoker@outlook.com>
2026-03-16 12:23:40 +0300

    added memory for the project

--- agentv1.py
+++ agentv1.py
@@ -2,23 +2,29 @@ from dotenv import load_dotenv
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
-from sympy import content
 from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.sqlite import SqliteSaver
 
 load_dotenv()
 
 aimodel = ChatOpenAI(model="gpt-4")
+memory = SqliteSaver.from_conn_string("memory.text")
+search = TavilySearchResults(max_results=3)
 
-search = TavilySearchResults(max_results=6)
 tavilysearch = [search]
 
 modelEngine = aimodel.bind_tools(tavilysearch)
 
-agent = create_react_agent(aimodel,tavilysearch)
+agent = create_react_agent(aimodel,tavilysearch,checkpointer=memory)
+
+config = {"configurable":{"thread_id":"abc1"}}
 
 if __name__ == "__main__":
-    response = agent.invoke(
-        {"messages": [HumanMessage(content="Who is software architect martin fowler")]},
-    )
-    for message in response["messages"]:
-        print(message.content)
+    while True:
+        user_input = input("> ")
+        for chunk in agent.stream(
+                { "messages": [HumanMessage(content=user_input)]},
+            config=config
+        ):
+            print(chunk)
+            print("----")
