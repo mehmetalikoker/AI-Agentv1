95943bd8e5b649ae8139bd1e552496e6dd1ce427
Mehmet <mehmetalikoker@outlook.com>
2026-03-16 11:37:05 +0300

    agent process added

--- main.py
+++ main.py
@@ -3,18 +3,22 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
 from sympy import content
+from langgraph.prebuilt import create_react_agent
 
 load_dotenv()
 
 aimodel = ChatOpenAI(model="gpt-4")
 
-
-search = TavilySearchResults(max_results=2)
+search = TavilySearchResults(max_results=6)
 tavilysearch = [search]
 
 modelEngine = aimodel.bind_tools(tavilysearch)
 
+agent = create_react_agent(aimodel,tavilysearch)
+
 if __name__ == "__main__":
-    response = modelEngine.invoke([HumanMessage(content="what is Mehmet Ali Köker job")])
-    print(response)
-    print(response.tool_calls)
+    response = agent.invoke(
+        {"messages": [HumanMessage(content="Who is software architect mehmet ali koker")]},
+    )
+    for message in response["messages"]:
+        print(message.content)
